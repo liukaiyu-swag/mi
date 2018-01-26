071460c807f6041d5be5f00fac7c6f87d5b14bc5
lky1106923807 <1106923807@qq.com>
2018-01-26 08:45:27 +0800

    mi

--- js/index.js
+++ js/index.js
@@ -1,203 +1,189 @@
+
 {
-	let list=document.querySelectorAll(".neirong .neirong-img1");
-	list.forEach(function(ele){
-		lunbo(ele);
-	});
-	function lunbo(ele){
-		let list=ele.querySelector(".neirong-img1 .list");
-		let list1=ele.querySelectorAll(".neirong-img1 .list1");
-		let left=ele.querySelector(".neirong-jiaotou2");
-		let right=ele.querySelector(".neirong .neirong-jiaotou3");
-		let yuan=ele.querySelectorAll(".neirong .neirong-yuan1");
-		let size=list1.length;
-		let n=0;
-		left.onclick=function(){
+	let imgs = document.querySelectorAll(".banner-img li");
+	let pagings = document.querySelectorAll(".xiaoyuandian li");
+	let banners = document.querySelector(".banner");
+	let next = document.querySelector(".jiantou-right");
+	let prev = document.querySelector(".jiantou-left");
+	pagings.forEach(function(ele, index) {
+		ele.onclick = function() {
+			for(let i = 0; i < pagings.length; i++) {
+				pagings[i].classList.remove("active");
+				imgs[i].classList.remove("active");
+			}
+			ele.classList.add("active");
+			imgs[index].classList.add("active");
+			n = index;
+		}
+	})
+	let n = 0;
+
+	function bannerdh(x) {
+		if(x) {
 			n--;
-			if(n<0){
-				n==0;
-				return;
-			}
-			list.style.marginLeft=-n*296+"px";
-			yuanColor();
-			
-		}
-		right.onclick=function(){
+		} else {
 			n++;
-			if(n>=size){
-				n=size-1;
-				return;
-			}
-			list.style.marginLeft=-n*296+"px";
-			yuanColor();
-			
 		}
-		yuan.forEach(function(ele,index){
-			console.log(ele);
-			ele.onclick=function(){
-				list.style.marginLeft=-index*296+"px";
-				n=index;
-				yuanColor();
-			}
-		});
-		function yuanColor(){
-			for(let i=0;i<size;i++){
-				yuan[i].classList.remove("neirong-yuan2");
-			}
-			console.log(n);
-			yuan[n].classList.add("neirong-yuan2")
+		if(n === -1) {
+			n = imgs.length - 1
+		}
+		if(n == imgs.length) {
+			n = 0
+		}
+		for(let i = 0; i < pagings.length; i++) {
+			pagings[i].classList.remove("active");
+			imgs[i].classList.remove("active");
+		}
+		pagings[n].classList.add("active");
+		imgs[n].classList.add("active");
+	}
+	let st = setInterval(bannerdh, 3000);
+	banners.onmouseover = function() {
+		clearInterval(st)
+	};
+	banners.onmouseout = function() {
+		st = setInterval(bannerdh, 3000);
+	}
+	let flat = true
+	next.onclick = function() {
+		if(flat) {
+			flat = false
+			bannerdh()
 		}
 	}
-}
-{
-	let all=document.querySelectorAll(".jiadian");
-	all.forEach(function(ele,index){
-		let list=ele.querySelectorAll(".jiadian .list");
-//		list[0].style.display="block";
-		let daohang=ele.querySelectorAll(".jiadian-a");
-		for(let i=0;i<daohang.length;i++){
-			daohang[i].onmouseover=function(){
-				console.log(list[i]);
-				for(let j=0;j<daohang.length;j++){
-					daohang[j].classList.remove("color");
-					list[j].style.display="none";
-				}
-				daohang[i].classList.add("color");
-				list[i].style.display="block";
-			}
+	prev.onclick = function() {
+		if(flat) {
+			flat = false
+			bannerdh(1)
 		}
-	});
-}
+	}
+	imgs.forEach(function(ele) {
+		ele.addEventListener("transitionend", function() {
+			flat = true
+		})
+	})
+} 
+//{
+//	let n=0
+//	$(".xiaoyuandian li").click(function(){
+//		let index=$(this).index()
+//		$(".xiaoyuandian li").removeClass("active");
+//		$(this).addClass("active")
+//		$(".banner-img li").removeClass("active").eq(index).addClass("active")
+//	})
+//	function move(){
+//		n++
+//		if(n===$(".xiaoyuandian li").length){
+//			n=0
+//		}
+//		$(".xiaoyuandian li").removeClass("active").eq(n).addClass("active")
+//		$(".banner-img li").removeClass("active").eq(n).addClass("active")
+//	}
+//	let st=setInterval(move,3000)
+//}
 {
-	let banner=document.querySelector(".banner");
-	let list=document.querySelectorAll(".banner-ul1 li");
-	let n=0;
-	let yuan=document.querySelectorAll(".banner .banner-yuan li");
-	let left=document.querySelector("#banner-jiantou1");
-	let right=document.querySelector("#banner-jiantou2");
-	console.log(left)
-	console.log(right)
-	function lunbo(){
+	let next = document.querySelector(".jiantou-you");
+	let prev = document.querySelector(".jiantou-zuo");
+	let danping = document.querySelector(".danping-xia");
+	next.onclick = function() {
+		this.classList.remove("active");
+		prev.classList.add("active");
+		danping.style.transform = "translateX(-1240px)";
+	}
+	prev.onclick = function() {
+		this.classList.remove("active");
+		next.classList.add("active");
+		danping.style.transform = "translatex(0)";
+	}
+	let n = 0;
+
+	function stay() {
 		n++;
-		if(n==5){
-			n=0;
+		if(n % 2 == 0) {
+			next.onclick()
+		} else {
+			prev.onclick()
 		}
-		if(n<0){
-			n=4;
-		}
-		for(let i=0;i<list.length;i++){
-			list[i].style.opacity=0;
-			yuan[i].classList.remove("style");
-		}
-		list[n].style.opacity=1;
-		yuan[n].classList.add("style");
 	}
-	let time=setInterval(lunbo,1000);
-	banner.onmouseover=function(){
-		clearInterval(time);
+	let st = setInterval(stay, 3000);
+	danping.onmouseover = function() {
+		clearInterval(st)
 	}
-	banner.onmouseout=function(){
-		time=setInterval(lunbo,1000);
+	danping.onmouseout = function() {
+		st = setInterval(stay, 3000);
 	}
-	left.onclick=function(){
-		n=n-2;
-		lunbo();
-	}
-	right.onclick=function(){
-		lunbo();
-	}
-	yuan.forEach(function(ele,index){
-		ele.onclick=function(){
-			n=index-1;
-			lunbo();
-		}
-	});
 }
-{
-	let big=document.querySelector("#danping-ul");
-	let list=document.querySelectorAll("#danping-ul li");
-	let right=document.querySelector(".danping .jiantou1");
-	let left=document.querySelector(".danping .jiantou2");
-	let n=0;
-	left.onclick=function(){
-		if(n==0){
-			return;
-		}
-		n=0;
-		big.style.marginLeft="0px";
-		left.style.color="";
-		left.style.fontWeight="normal";
-		right.style.color="#ccc";
-		right.style.fontWeight="bold";
-	}
-	right.onclick=function(){
-		if(n==1){
-			return;
-		}
-		n=1;
-		big.style.marginLeft="-1226px";
-		right.style.color="";
-		right.style.fontWeight="normal";
-		left.style.color="#ccc";
-		left.style.fontWeight="bold";
-	}
-	function move(){
-		if(n==0){
-			big.style.marginLeft="-1226px";
-			n=1;
-			right.style.color="";
-			right.style.fontWeight="normal";
-			left.style.color="#ccc";
-			left.style.fontWeight="bold";
-			return;
-		}
-		if(n==1){
-			n=0;
-			big.style.marginLeft="0px";
-			left.style.color="";
-			left.style.fontWeight="normal";
-			right.style.color="#ccc";
-			right.style.fontWeight="bold";
-		}
-	}
-	let time=setInterval(move,5000);
-	left.onmouseenter=()=>{
-		clearInterval(time);
-		if(n==1){
-			left.style.color="#FF8D40";
-		}
-	}
-	right.onmouseenter=()=>{
-		clearInterval(time);
-		if(n==0){
-			right.style.color="#FF8D40";
-		}
-	}
-	left.onmouseleave=right.onmouseleave=function(){
-		time=setInterval(move,5000);
-		left.style.color="";
-	}
-	right.onmouseleave=function(){
-		time=setInterval(move,5000);
-		right.style.color="";
-	}
-	
-}
-{
-	let list=document.querySelectorAll(".banner-daohang-ul1 li");
-	let list1=document.querySelectorAll(".banner .children");
-	let list2;
-	list.forEach(function(ele,index){
-		list1[index].onmouseenter=ele.onmouseenter=function(){ 
-			for(let i=0;i<list.length;i++){
-				list1[i].style.display="none";
+{   
+	let list=document.querySelectorAll(".nx .nierongxiao")
+			list.forEach(function(ele){
+				ggb(ele)
+			})
+	function ggb(lyq){
+	let inner=lyq.querySelector(".neirongkuangjia")
+	let prev=lyq.querySelector(".next")
+	let next=lyq.querySelector(".prev")
+	let item=lyq.querySelectorAll(".neirongxiaokuangjia")
+	let pages=lyq.querySelectorAll(".diandian li")
+	let items=item.length
+	let n = 0;
+	console.log(inner)
+	next.onclick = function() {
+		n++
+		if(n >= items) {
+			n = items - 1
+				return
 			}
-			list1[index].style.display="block";
-			list2=list1[index];
+			inner.style.marginLeft = -n * 300 + "px"
+			for(let i = 0; i < pages.length; i++) {
+				pages[i].classList.remove("active")
+			}
+				pages[n].classList.add("active")
+			}
+			prev.onclick = function() {
+				n--
+				if(n < 0) {
+				    n = 0
+					return
+				}
+		    inner.style.marginLeft = -n * 300 + "px"
+			for(let i = 0; i < pages.length; i++) {
+				pages[i].classList.remove("active")
+			}
+			pages[n].classList.add("active")
+			}
+			pages.forEach(function(ele, index) {
+				let m = index
+				ele.onclick = function() {
+					for(let i = 0; i < pages.length; i++) {
+						pages[i].classList.remove("active")
+					}
+					ele.classList.add("active")
+					inner.style.marginLeft = -m * 300 + "px"
+					n = index
+				}
+			})	
 		}
-		list1[index].onmouseleave=ele.onmouseleave=()=>{
-			list2.style.display="none";
+}
+{
+	let list=document.querySelectorAll(".beijing .jiadian")
+			list.forEach(function(ele){
+				jiadian(ele)
+			})
+	function jiadian(jd){
+		let pagers=jd.querySelectorAll('.youbuyou .text-remen')
+	let imgs=jd.querySelectorAll('.jiadian_bottom .jiadaindi')
+	pagers.forEach(function(ele,index){
+		ele.onmouseover=function(){
+			for(let i=0;i<imgs.length;i++){
+				pagers[i].classList.remove('active');
+				imgs[i].classList.remove('active')
+				
+			}
+			this.classList.add('active');
+			imgs[index].classList.add('active')
 		}
-	});
+	})
+	}
 }
 {
 	let list1=document.querySelectorAll(".daohang-ul .figure img");
@@ -247,5 +233,72 @@
 		inputText.style.display="";
 		div.style.display="";
 		img.style.border="";
+	}
+}
+{
+	let list=document.querySelectorAll(".banner-left li");
+	let list1=document.querySelectorAll(".banner .children");
+	let list2;
+	list.forEach(function(ele,index){
+		list1[index].onmouseenter=ele.onmouseenter=function(){ 
+			for(let i=0;i<list.length;i++){
+				list1[i].style.display="none";
+			}
+			list1[index].style.display="block";
+			list2=list1[index];
+		}
+		list1[index].onmouseleave=ele.onmouseleave=()=>{
+			list2.style.display="none";
+		}
+	});
+}
+{
+	let list=document.querySelectorAll(".neirong .neirong-img1");
+	list.forEach(function(ele){
+		lunbo(ele);
+	});
+	function lunbo(ele){
+		let list=ele.querySelector(".neirong-img1 .list");
+		let list1=ele.querySelectorAll(".neirong-img1 .list1");
+		let left=ele.querySelector(".neirong-jiaotou2");
+		let right=ele.querySelector(".neirong .neirong-jiaotou3");
+		let yuan=ele.querySelectorAll(".neirong .neirong-yuan1");
+		let size=list1.length;
+		let n=0;
+		left.onclick=function(){
+			n--;
+			if(n<0){
+				n=0;
+				return;
+			}
+			list.style.marginLeft=-n*296+"px";
+			yuanColor();
+			
+		}
+		right.onclick=function(){
+			n++;
+			if(n>=size){
+				n=size-1;
+				return;
+			}
+			list.style.marginLeft=-n*296+"px";
+			yuanColor();
+			
+		}
+		yuan.forEach(function(ele,index){
+			console.log(ele);
+			ele.onclick=function(){
+				list.style.marginLeft=-index*296+"px";
+				n=index;
+				yuanColor();
+			}
+		});
+		function yuanColor(){
+			for(let i=0;i<size;i++){
+				yuan[i].classList.remove("neirong-yuan2");
+			}
+			console.log(n);
+			yuan[n].classList.add("neirong-yuan2")
+		}
 	}
 }
